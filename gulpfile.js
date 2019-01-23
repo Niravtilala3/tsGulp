c48646a5d9275d3264dd2208e1e14a7211973c37
unknown <nirav.tilala@64SV7Q2.ad.rajkot.searce.local>
2019-01-23 10:29:56 +0530

    add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task("default", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+// const tsProject = ts.createProject("tsconfig.json");
+
+// gulp.task("default", function () {
+//     return tsProject.src()
+//         .pipe(tsProject())
+//         .js.pipe(gulp.dest("dist"));
+// });
+gulp.task('script',function(){
+    return gulp.src('src/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            noImplicitAny: true
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('watch', ['script'], function() {
+    gulp.watch('src/**/*.ts', ['script']);
+});
+
+gulp.task('default', ['watch']);
